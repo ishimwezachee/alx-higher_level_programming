8c1838990f4ce838c0413582ff0426fe929d9056
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-30 03:55:25 -0800

    add element

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3 
 def safe_print_list_integers(my_list=[], x=0):
+    index = 0
     for i in range(x):
         try:
             if isinstance(my_list[i],int):
-                print(my_list[i])
+                print(my_list[i], end="")
+                index +=1
         except Exception as error:
             print(error)
+    print()
+    return index
 
 
