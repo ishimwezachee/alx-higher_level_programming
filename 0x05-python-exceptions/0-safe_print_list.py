adb98150cc6d5e28d78341a7be4795782aa47595
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-30 01:06:46 -0800

    fix error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    num = 0;
     try:
         for i in range(x):
             print(my_list[i], end=" ")
+            index +=1
     except IndexError:
-        print("Your Index is out of bound")
+        break
     print("")
+    return index
 
