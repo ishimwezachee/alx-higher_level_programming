c69d9d548549d5b5f29e0ed0284f0c8333a56cda
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-30 01:21:34 -0800

    fix error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     index = 0;
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end='')
             index +=1
     except Exception as error:
-        break
+        print(error)
     print("")
     return index
 
