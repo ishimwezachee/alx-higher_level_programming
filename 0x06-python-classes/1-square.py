40fa9d802affef59ff004bbdbf6660752a41547d
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-30 13:49:47 -0800

    constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """ represent a square with 
+    attribute:  __size (int): size of a side of the square"""
+    __init__(self,size):
+        """"__size that represent the attributes of a Square class"""
+        self.__size = size
